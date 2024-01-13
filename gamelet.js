d03294bd5e5655d39c5c7add8016f3b6846fb737
James Pask <james@jpask.com>
2024-01-13 21:26:47 +0000

    first commit

---
+++ gamelet.js
@@ -0,0 +1,32 @@
+const ball = document.getElementById('ball');
+document.addEventListener('keydown', handleKeyPress);
+let position = 0;
+let verticalPosition = 0;
+
+function handleKeyPress(e) {
+  if (e.code === 'ArrowLeft') {
+    position = position - 50;
+  }
+  if (e.code === 'ArrowRight') {
+    position = position + 50;
+  }
+  if (position < 0) {
+    position = 0;
+  }
+  if (e.code === 'ArrowUp') {
+    verticalPosition = verticalPosition - 50;
+  }
+  if (e.code === 'ArrowDown') {
+    verticalPosition = verticalPosition + 50;
+  }
+  if (verticalPosition < 0) {
+    verticalPosition = 0;
+  }
+
+  refresh();
+}
+
+function refresh() {
+  ball.style.left = position + 'px';
+  ball.style.top = verticalPosition + 'px';
+}
